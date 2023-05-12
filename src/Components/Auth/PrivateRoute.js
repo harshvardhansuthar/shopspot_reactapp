36363116528eb61e62735c201517e8280c9d121e
yogeshjoshi20 <yogesh@mandelo.in>
2023-05-12 15:12:11 +0530

    add

--- src/Components/Auth/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Cookies from "js-cookie";
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRoute = () => {
-  const token = Cookies.get("token");
-  return <>{token ? <Outlet /> : <Navigate to={"/"} />}</>;
-};
-export default PrivateRoute;
